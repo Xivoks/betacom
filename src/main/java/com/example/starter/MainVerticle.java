b2893efdc733f700dbf1ca8f3d7aa3f008830d31
Janek <j.brunow@lastlevel.pl>
2023-11-29 22:55:54 +0100

    changing the way a record is added to the database, adding by sending an appropriate request.

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -5,33 +5,26 @@ import com.example.starter.controller.ItemsController;
 import com.example.starter.controller.LoginController;
 import com.example.starter.controller.RegisterController;
 import com.example.starter.database.MongoDatabaseManager;
-import com.mongodb.client.MongoClient;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Context;
-import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
-import org.bson.Document;
 
 public class MainVerticle extends AbstractVerticle {
 
-  private MongoClient mongoClient;
+  private MongoDatabaseManager databaseManager;
 
-  @Override
-  public void init(Vertx vertx, Context context) {
-    super.init(vertx, context);
-    MongoDatabaseManager databaseManager = new MongoDatabaseManager();
-    DataController dataController = new DataController(databaseManager);
-
-    String collectionName = "betacom";
-    Document document = new Document("key", "value");
-    dataController.insertData(collectionName, document);
+  public MainVerticle() {
   }
 
   @Override
   public void start() {
+    databaseManager = new MongoDatabaseManager();
+
     HttpServer server = vertx.createHttpServer();
     Router router = Router.router(vertx);
+
+    DataController dataController = new DataController(databaseManager);
+    router.post("/add-data").handler(dataController::handleAddData);
 
     LoginController.register(router);
     RegisterController.register(router);
@@ -42,8 +35,8 @@ public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void stop() {
-    if (mongoClient != null) {
-      mongoClient.close();
+    if (databaseManager != null) {
+      databaseManager.close();
     }
   }
 }
--- src/main/java/com/example/starter/controller/DataController.java
+++ src/main/java/com/example/starter/controller/DataController.java
@@ -3,10 +3,11 @@ package com.example.starter.controller;
 import com.example.starter.database.MongoDatabaseManager;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoDatabase;
+import io.vertx.ext.web.RoutingContext;
 import org.bson.Document;
 
 public class DataController {
-  private MongoDatabaseManager databaseManager;
+  private final MongoDatabaseManager databaseManager;
 
   public DataController(MongoDatabaseManager databaseManager) {
     this.databaseManager = databaseManager;
@@ -16,6 +17,18 @@ public class DataController {
     try (MongoClient mongoClient = databaseManager.getMongoClient()) {
       MongoDatabase database = mongoClient.getDatabase("betacom");
       database.getCollection(collectionName).insertOne(document);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
+
+  public void handleAddData(RoutingContext context) {
+    String collectionName = "betacom";
+    Document document = new Document("key", "value");
+    insertData(collectionName, document);
+
+    context.response()
+      .putHeader("content-type", "application/json")
+      .end("Dane zostały dodane do bazy");
   }
 }
