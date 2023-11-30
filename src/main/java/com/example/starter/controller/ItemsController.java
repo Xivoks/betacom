dec36a27d06bf0180b3a6f51575ed4bc1c7d76b2
Janek <j.brunow@lastlevel.pl>
2023-11-30 23:34:36 +0100

    changes related with added handleTokenAuthentication

--- src/main/java/com/example/starter/controller/ItemsController.java
+++ src/main/java/com/example/starter/controller/ItemsController.java
@@ -2,6 +2,8 @@ package com.example.starter.controller;
 
 import com.example.starter.database.MongoDatabaseManager;
 import com.mongodb.client.MongoCollection;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.auth.User;
 import io.vertx.ext.auth.authentication.TokenCredentials;
@@ -26,78 +28,70 @@ public class ItemsController {
   public void register(Router router) {
     router.post("/secure/items").handler(this::createItem);
     router.get("/secure/items").handler(this::getItems);
-
   }
 
   private void createItem(RoutingContext ctx) {
-    String authorizationHeader = ctx.request().getHeader("Authorization");
-    if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-      String token = authorizationHeader.substring(7);
+    handleTokenAuthentication(ctx, authResult -> {
+      if (authResult.succeeded()) {
+        User user = authResult.result();
+        String userId = user.principal().getString("sub");
 
-      jwtAuth.authenticate(new TokenCredentials(token), authResult -> {
-        if (authResult.succeeded()) {
-          User user = authResult.result();
-          String userId = user.principal().getString("sub");
+        JsonObject requestBody = ctx.getBodyAsJson();
+        if (requestBody != null && requestBody.containsKey("itemName")) {
+          String itemName = requestBody.getString("itemName");
 
-          JsonObject requestBody = ctx.getBodyAsJson();
-          if (requestBody != null) {
-            String itemName = requestBody.getString("itemName");
+          Document itemDocument = new Document()
+            .append("id", UUID.randomUUID().toString())
+            .append("owner", userId)
+            .append("name", itemName);
 
-            Document itemDocument = new Document()
-              .append("id", UUID.randomUUID().toString())
-              .append("owner", userId)
-              .append("name", itemName);
+          databaseManager.getCollection("items").insertOne(itemDocument);
 
-            databaseManager.getCollection("items").insertOne(itemDocument);
+          JsonObject response = new JsonObject()
+            .put("message", "Przedmiot został utworzony")
+            .put("item", itemDocument);
 
-            JsonObject response = new JsonObject()
-              .put("message", "Przedmiot został utworzony")
-              .put("item", itemDocument);
-
-            ctx.response().putHeader("content-type", "application/json")
-              .setStatusCode(201)
-              .end(response.encode());
-          } else {
-            ctx.response().setStatusCode(400).end("Invalid request body format");
-          }
+          ctx.response().putHeader("content-type", "application/json")
+            .setStatusCode(201)
+            .end(response.encode());
         } else {
-          ctx.response().setStatusCode(401).end("Authentication failed");
+          ctx.response().setStatusCode(400).end("Invalid request body format");
         }
-      });
-    } else {
-      ctx.response().setStatusCode(401).end("Unauthorized");
-    }
+      } else {
+        ctx.response().setStatusCode(401).end("Authentication failed");
+      }
+    });
   }
 
   private void getItems(RoutingContext ctx) {
+    handleTokenAuthentication(ctx, authResult -> {
+      if (authResult.succeeded()) {
+        User user = authResult.result();
+        String userId = user.principal().getString("sub");
+
+        MongoCollection<Document> itemsCollection = databaseManager.getCollection("items");
+        Document query = new Document("owner", userId);
+        List<Document> userItems = itemsCollection.find(query).into(new ArrayList<>());
+
+        JsonObject itemsResponse = new JsonObject()
+          .put("items", userItems);
+
+        ctx.response().putHeader("content-type", "application/json")
+          .setStatusCode(200)
+          .end(itemsResponse.encode());
+      } else {
+        ctx.response().setStatusCode(401).end("Authentication failed");
+      }
+    });
+  }
+
+  private void handleTokenAuthentication(RoutingContext ctx, Handler<AsyncResult<User>> handler) {
     String authorizationHeader = ctx.request().getHeader("Authorization");
     if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
       String token = authorizationHeader.substring(7);
-
-      jwtAuth.authenticate(new TokenCredentials(token), authResult -> {
-        if (authResult.succeeded()) {
-          User user = authResult.result();
-          String userId = user.principal().getString("sub");
-
-          MongoCollection<Document> itemsCollection = databaseManager.getCollection("items");
-          Document query = new Document("owner", userId);
-          List<Document> userItems = itemsCollection.find(query).into(new ArrayList<>());
-
-          JsonObject itemsResponse = new JsonObject()
-            .put("items", userItems);
-
-          ctx.response().putHeader("content-type", "application/json")
-            .setStatusCode(200)
-            .end(itemsResponse.encode());
-        } else {
-          ctx.response().setStatusCode(401).end("Authentication failed");
-        }
-      });
+      jwtAuth.authenticate(new TokenCredentials(token), handler);
     } else {
       ctx.response().setStatusCode(401).end("Unauthorized");
     }
   }
-
-
-
 }
